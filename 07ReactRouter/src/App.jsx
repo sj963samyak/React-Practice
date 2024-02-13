3c3c7899408df6a06c340cd5e237ab0fcc4c2a99
samyak <sj963samyak@gmail.com>
2024-02-13 21:45:35 +0530

    React router start

---
+++ 07ReactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-blue-600">My name is samyak jain</h1>
+      <h2>I am working in openAI</h2>
+    </>
+  );
+}
+
+export default App;
